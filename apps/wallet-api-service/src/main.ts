289f984b7657b2c6505522002e69afcfb8e9fa8c
damorao271 <damorao27@gmail.com>
2025-09-28 15:36:23 -0400

    Listen on port 3000 from the container

--- apps/wallet-api-service/src/main.ts
+++ apps/wallet-api-service/src/main.ts
@@ -5,7 +5,7 @@ import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(WalletApiServiceModule);
   const configService = app.get(ConfigService);
-  const port = configService.get<number>('PORT_WALLET_API') || 3000;
+  const port = configService.get<number>('CONTAINER_PORT') || 3000;
   await app.listen(port);
 }
 bootstrap();
--- apps/wallet-db-service/src/main.ts
+++ apps/wallet-db-service/src/main.ts
@@ -1,11 +1,11 @@
 import { NestFactory } from '@nestjs/core';
-import { WalletDbServiceModule } from './wallet-db-service.module' 
+import { WalletDbServiceModule } from './wallet-db-service.module';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(WalletDbServiceModule);
   const configService = app.get(ConfigService);
-  const port = configService.get<number>('PORT_WALLET_DB') || 3000;
+  const port = configService.get<number>('CONTAINER_PORT') || 3000;
   await app.listen(port);
 }
 bootstrap();
