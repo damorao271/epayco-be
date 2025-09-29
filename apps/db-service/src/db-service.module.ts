33fbe0d003afec91878408c29a8037010ee16af5
damorao271 <damorao27@gmail.com>
2025-09-28 21:44:32 -0400

    Create basic entities and resources

--- apps/db-service/src/db-service.module.ts
+++ apps/db-service/src/db-service.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { DbServiceController } from './db-service.controller';
 import { DbServiceService } from './db-service.service';
 import { ConfigModule } from '@nestjs/config';
+import { WalletModule } from './wallet/wallet.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true, // makes config available everywhere
     }),
+    WalletModule,
   ],
   controllers: [DbServiceController],
   providers: [DbServiceService],
